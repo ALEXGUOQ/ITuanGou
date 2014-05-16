6f6428f060b2e2afea26a9d86ea8a1311eb1c004
azxfire <azxfire@gmail.com>
2014-05-15 18:47:27 -0700

    Initial Commit

---
+++ ITuanGou/TGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TGAppDelegate.h
+//  ITuanGou
+//
+//  Created by wangtao on 14-5-15.
+//  Copyright (c) 2014年 JobsTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ITuanGou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ITuanGou
+//
+//  Created by wangtao on 14-5-15.
+//  Copyright (c) 2014年 JobsTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGAppDelegate class]));
+    }
+}
